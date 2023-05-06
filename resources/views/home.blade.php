c62e7dab4ab74a885fe05b330197f1bdfdd83620
P4anix <pancok_1@yahoo.it>
2023-05-06 12:17:53 +0200

    Treni odierni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,5 +43,43 @@
     </table>
         </div>
 
+        <div class="container">
+            <h2 class="text-center p-5 text-white">Partenza Treni Odierni</h2>
+             <table class="table">
+        <thead>
+            <tr class="table-info">
+                <th scope="col">ID Treno</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">Carrozze</th>
+                <th scope="col">Compagnia</th>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario Partenze</th>
+                <th scope="col">Orario Arrivo</th>
+                <th scope="col">Puntuale</th>
+                <th scope="col">Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($todays as $today)
+                <tr class="text-white">
+                    <th scope="row">{{ $today['id']}}</th>
+                    <td>{{ $today['train_code']}}</td>
+                    <td>{{ $today['num_carriages']}}</td>
+                    <td>{{ $today['company']}}</td>
+                    <td>{{ ucfirst($today['departure_station']) }}</td>
+                    <td>{{ ucfirst($today['arrival_station']) }}</td>
+                    <td>{{ $today['departure_time']}}</td>
+                    <td>{{ $today['arrival_time']}}</td>
+                    <td>{{ $today['in_time'] == 0 ? 'No' : 'Si' }}</td>
+                    <td>{{ $today['cancelled'] == 0 ? 'No' : 'Si'  }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+            
+
+        </div>
+
     </body>
 </html>
