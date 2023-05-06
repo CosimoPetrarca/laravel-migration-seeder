24c78f11f33be9bf081e2666cb8e343ffec28143
P4anix <pancok_1@yahoo.it>
2023-05-06 10:23:23 +0200

    tabella creata

---
+++ database/migrations/2023_05_06_073543_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 20);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->dateTimeTz('departure_time'); 
+            $table->dateTimeTz('arrival_time');
+            $table->string('train_code', 10);
+            $table->unsignedTinyInteger('num_carriages');
+            $table->boolean('in_time')->default(1);
+            $table->boolean('cancelled')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
